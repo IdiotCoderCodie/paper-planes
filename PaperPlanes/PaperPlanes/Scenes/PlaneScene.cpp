e44615f82021acaf328f9b1ed198ef4b632c1f5d
Codie Collinge <c.j.collinge@live.co.uk>
2013-12-13 18:24:45 +0000

    Fixed silly problem in PlaneScene, was creating a "testScene" within it.

--- PaperPlanes/PaperPlanes/Scenes/PlaneScene.cpp
+++ PaperPlanes/PaperPlanes/Scenes/PlaneScene.cpp
@@ -14,32 +14,26 @@ PlaneScene::PlaneScene(const std::string& name, SceneManager* sceneMgr)
     : Scene(name, sceneMgr)
 {
     D3D& d3d = GetParent().GetD3DInstance();
-    // TESTING. ALL THIS IN HERE IS FOR TESTING. NEED TO GET IT OUT.
+    // *************** TESTING. ALL THIS IN HERE IS FOR TESTING. *************** 
 	Shader* shader = new Shader();
 
-    Scene* testScene = new Scene(std::string("testScene"), sceneMgr);
-
-    sceneMgr->AddScene(testScene);
     // Cube entity.
-    Entity* cubeEntity = new Entity(*testScene, std::string("testEntity1"));
+    Entity* cubeEntity = new Entity(*this, std::string("testEntity1"));
 
-    testScene->AddEntity(cubeEntity);
+    AddEntity(cubeEntity);
 
 	Texture* texture = new Texture(d3d, L"Assets\\Textures\\tim.dds");
     VisualMeshComponent* meshComp 
-        = new VisualMeshComponent(d3d, std::string("Assets\\Models\\cube.obj"), *shader, *texture); 
+        = new VisualMeshComponent(d3d, std::string("Assets\\Models\\cubeInv.obj"), *shader, *texture); 
     
     cubeEntity->MoveForward(0.0f);
     cubeEntity->MoveRight(1.0f);
     meshComp->SetParent(*cubeEntity);
 
     cubeEntity->SetComponent(meshComp);
-    /*cubeEntity->SetComponent(new PhysicsComponent(1.0f, glm::vec3(0.0f), glm::vec3(0.0f),
-                                                  glm::vec3(10.0f, 5.0f, 15.0f), 
-                                                  glm::vec3(0.0f, 0.0f, 0.000f)));*/
 
 
-    Entity* cameraEntity = new Entity(*testScene, std::string("camEntity"));
+    Entity* cameraEntity = new Entity(*this, std::string("camEntity"));
     float aspect = GetParent().GetD3DInstance().GetScreenWidth() 
                         / (float)GetParent().GetD3DInstance().GetScreenHeight();
     CameraComponent* camComp = new PerspectiveCamComponent(60.0f, aspect, 0.1f, 1000.0f);
@@ -53,18 +47,18 @@ PlaneScene::PlaneScene(const std::string& name, SceneManager* sceneMgr)
     camComp->SetParent(*cameraEntity);
     cameraEntity->SetComponent(new FreeRoamFpComponent(10.0f, 50.0f, 50.0f));
 	
-    testScene->AddEntity(cameraEntity);
+    AddEntity(cameraEntity);
 
 
     // TESTING BITMAP
-    Entity* bmpEntity = new Entity(*testScene, std::string("bmpEntity"));
+    Entity* bmpEntity = new Entity(*this, std::string("bmpEntity"));
     Shader* newShader = new Shader();
     Bitmap* bmp = new Bitmap(d3d, *texture, 256, 256, 
                              GetParent().GetD3DInstance().GetScreenWidth(), 
                              GetParent().GetD3DInstance().GetScreenHeight());
 
     bmpEntity->SetComponent(new VisualBitmapComponent(d3d, *newShader, *bmp));
-    testScene->AddEntity(bmpEntity);
+    AddEntity(bmpEntity);
     // Some lolz.
     /*bmpEntity->SetComponent(new PhysicsComponent(1.0f, glm::vec3(-10.0f, 0.0f, 0.0f), 
                             glm::vec3(0.1f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)));*/
