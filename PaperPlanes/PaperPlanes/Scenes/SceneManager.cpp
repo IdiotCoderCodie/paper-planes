04c1be851fe8ba4172ab0b904857536ed1b04e60
IdiotCoderCodie <c.j.collinge@live.co.uk>
2014-01-12 16:36:25 +0000

    Oops, committed build which failed to run. Fixed.

--- PaperPlanes/PaperPlanes/Scenes/SceneManager.cpp
+++ PaperPlanes/PaperPlanes/Scenes/SceneManager.cpp
@@ -30,7 +30,6 @@ SceneManager& SceneManager::operator=(const SceneManager& other)
 {
     m_Scenes = other.m_Scenes;
     m_ActiveScene = other.m_ActiveScene;
-    m_d3d = other.m_d3d;
     return *this;
 }
 
